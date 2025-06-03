a024586228b030529410b60b84cc59f4e2a9c845
viperrTopTopMoney <165705201+viperrTopTopMoney@users.noreply.github.com>
2025-06-04 02:42:05 +0300

    blogikum

--- static/settings.py
+++ static/settings.py
@@ -1,5 +1,11 @@
+# blogicum/settings.py
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig',
-    'pages.apps.PagesConfig',
-    # остальные...
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',        # ← Добавить
+    'pages.apps.PagesConfig',      # ← Добавить
 ]
